945c79ce5da8b8944dbd5735d61f3a9eeee26247
Yousef Wadi <yousef@nestrom.com>
2022-02-21 14:00:44 +0200

    Logs moved to STDOUT

--- Logger/Logger.go
+++ Logger/Logger.go
@@ -4,14 +4,10 @@ import (
 	"os"
 
 	log "github.com/sirupsen/logrus"
-	"github.com/spf13/viper"
 )
 
 func Init() {
-	f, err := os.OpenFile(viper.GetString("crimson_settings.log_file"), os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
-	if err != nil {
-		log.Fatalf("error opening file: %v", err)
-	}
 	log.SetFormatter(&log.JSONFormatter{})
-	log.SetOutput(f)
+
+	log.SetOutput(os.Stdout)
 }
